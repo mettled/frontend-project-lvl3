b4a1e7fe87e777e8f32572faca655de3d9e98b44
mettled <smettled@gmail.com>
2020-04-07 00:53:27 -0700

    basic progect

---
+++ Makefile
@@ -0,0 +1,11 @@
+install:
+	npm install
+
+start:
+	npx babel-node 
+
+lint:
+	npx eslint .
+
+dev:
+	npx webpack
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        'index': path.resolve(__dirname, 'src'),
+    },
+    output: {
+        filename: `[name].js`,
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {   test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            }
+        ]
+    }
+}
