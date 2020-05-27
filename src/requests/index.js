36e08d06d473cd5fea0b0d893f99f4d57c0c0be2
mettled <smettled@gmail.com>
2020-05-27 04:03:23 -0700

    fix codeclimate

--- src/requests/index.js
+++ src/requests/index.js
@@ -8,28 +8,28 @@ import { addIDToArticles, addIDToSource, getNewArticles } from '../helpers';
 
 const PERIOD_REQUEST = 5000;
 
-const addSource = (state, link) => (
+const addSource = ({ sources: sourcesState, articles: articlesState, form }, link) => (
   makeRequest(link)
     .then(({ data: { contents, status: { url } } }) => {
       const parsedContent = parse(contents);
 
       if (!parsedContent) {
-        state.form.status = STATUS.ERROR;
-        state.form.error = ERRORS.NOFEED;
+        form.status = STATUS.ERROR;
+        form.error = ERRORS.NOFEED;
         return;
       }
       const { source, articles } = parsedContent;
       const sourceID = uniqueId();
       const sourcesWithID = addIDToSource(sourceID, { ...source, link: url });
       const articlesWithID = addIDToArticles(sourceID, articles);
-      state.sources.push(sourcesWithID);
-      state.articles.push(...articlesWithID);
-      state.form.status = STATUS.ADDED;
-      state.form.error = ERRORS.EMPTY;
+      sourcesState.push(sourcesWithID);
+      articlesState.push(...articlesWithID);
+      form.status = STATUS.ADDED;
+      form.error = ERRORS.EMPTY;
     })
     .catch(() => {
-      state.form.status = STATUS.ERROR;
-      state.form.error = ERRORS.NETWORK;
+      form.status = STATUS.ERROR;
+      form.error = ERRORS.NETWORK;
     })
 );
 
@@ -39,20 +39,19 @@ const updateSources = (state) => {
   const sourcesID = sources.map(({ id }) => id);
   return Promise.allSettled(requests)
     .then((responses) => (
-      responses
-        .forEach((response, index) => {
-          if (response.status === 'rejected') {
-            sources[index].status = false;
-            return;
-          }
-          const { articles } = parse(response.contents);
-          const newArticles = getNewArticles(articles, state.articles);
-          if (newArticles.length === 0) {
-            return;
-          }
-          const articlesWithID = addIDToArticles(sourcesID[index], newArticles);
-          state.articles.push(...articlesWithID);
-        })
+      responses.forEach((response, index) => {
+        if (response.status === 'rejected') {
+          sources[index].status = false;
+          return;
+        }
+        const { articles } = parse(response.contents);
+        const newArticles = getNewArticles(articles, state.articles);
+        if (newArticles.length === 0) {
+          return;
+        }
+        const articlesWithID = addIDToArticles(sourcesID[index], newArticles);
+        state.articles.push(...articlesWithID);
+      })
     ))
     .catch(() => {
       state.form.status = STATUS.ERROR;
